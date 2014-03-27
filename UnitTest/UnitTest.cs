0d803f3d7c10f7684957c2edde5a9b981b612a83
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-03-27 09:56:31 -0700

    Remove & sort using statements.

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Wsdot.Traffic;
 using Wsdot.Traffic.Client;
 using WsdotTravelerInfoContracts.Ferries.Terminals;
